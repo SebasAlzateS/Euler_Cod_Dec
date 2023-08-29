9df7507718a8cc67f017f9fbf936fcfc29a53d5e
Fernando García Guiral <143369939+fernandogarciaguiral@users.noreply.github.com>
2023-08-29 18:04:34 -0500

    Update Edo_euler.py

--- Edo_euler.py
+++ Edo_euler.py
@@ -10,9 +10,9 @@ DANIELA DEOSSA SANCHEZ
 import functions as ft
 
 print("Solución de una Ecuación Diferencial Ordinaria mediante el Método de Euler")
-x0 = ft.dato_numerico("Ingrese el valor inicial de x (x0): ")
-y0=ft.dato_numerico("Ingrese el valor inicla de y (y0): ")
-h=ft.dato_numerico( "Ingrese el tamaño del paso (h): ")
+x0 = ft.dato_numerico2("Ingrese el valor inicial de x (x0): ")
+y0=ft.dato_numerico2("Ingrese el valor inicla de y (y0): ")
+h=ft.dato_numerico2( "Ingrese el tamaño del paso (h): ")
 n=ft.dato_entero_positivo( "Ingrese el número de pasos (n): ")
 edo_input = ft.validar_entrada()
 
